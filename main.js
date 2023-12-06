d53a37a223d24ece6f5baef930415544d63b2f4f
Dhriti-Malhotra <134493853+Dhriti-Malhotra@users.noreply.github.com>
2023-12-06 17:45:53 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ camera = document.getElementById("camera");
 
 Webcam.attach('#camera');
 
-function snapshot()
+function take_snapshot()
 {
     Webcam.snap(function(data_uri)  {
         document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
@@ -19,3 +19,24 @@ function snapshot()
 console.log('ml5 version:', ml5.version);
 
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/jc8jLbxCq/', modelLoaded);
+
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error)  {
+        console.error(error);
+    }   else    {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
